ede812d72b758f603070c1d52e2e3fe850859851
KasumiiUwU <p.khoa62@gmail.com>
2024-05-17 00:15:03 +0700

    edit entity

--- FuStudy_Repository/Entity/Blog.cs
+++ FuStudy_Repository/Entity/Blog.cs
@@ -12,13 +12,12 @@ namespace FuStudy_Repository.Entity
     public class Blog
     {
         [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public long Id { get; set; }
 
         public long UserId { get; set; }
 
         [Required]
-        public string BlogContent { get; set; }
+        public string Content { get; set; }
 
         [Required]
         public string Image { get; set; }
