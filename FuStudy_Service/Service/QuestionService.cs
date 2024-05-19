e800fe5ffe8a499910f1a208b0b1d4b17ef312d7
KasumiiUwU <p.khoa62@gmail.com>
2024-05-19 16:59:35 +0700

    add custom response and custom exception in QuestionController.cs, add required field, try catch.

--- FuStudy_Service/Service/QuestionService.cs
+++ FuStudy_Service/Service/QuestionService.cs
@@ -7,6 +7,7 @@ using FuStudy_Repository.Repository;
 using FuStudy_Service.Interface;
 using System;
 using System.Collections.ObjectModel;
+using Tools;
 
 namespace FuStudy_Service.Service
 {
@@ -36,6 +37,14 @@ namespace FuStudy_Service.Service
 
         public async Task<QuestionResponse> CreateQuestionAsync(QuestionRequest questionRequest)
         {
+            if (_unitOfWork.StudentRepository.GetByID(questionRequest.StudentId) == null)
+            {
+                throw new CustomException.DataNotFoundException($"Student with this {questionRequest.StudentId} not found!");
+            }
+            if (_unitOfWork.CategoryRepository.GetByID(questionRequest.CategoryId) == null)
+            {
+                throw new CustomException.DataNotFoundException($"Category with this {questionRequest.CategoryId} not found!");
+            }
 
             var question = _mapper.Map<Question>(questionRequest);
             _unitOfWork.QuestionRepository.Insert(question);
@@ -50,7 +59,7 @@ namespace FuStudy_Service.Service
 
             if (question == null)
             {
-                throw new Exception($"Question with ID: {questionId} not found");
+                throw new CustomException.DataNotFoundException($"Question with ID: {questionId} not found");
             }
 
             _mapper.Map(questionRequest, question);
@@ -67,7 +76,7 @@ namespace FuStudy_Service.Service
 
             if (deletedQuestion == null)
             {
-                throw new Exception($"Question with ID: {questionId} not found");
+                throw new CustomException.DataNotFoundException($"Question with ID: {questionId} not found");
             }
             
             _unitOfWork.QuestionRepository.Delete(deletedQuestion);
