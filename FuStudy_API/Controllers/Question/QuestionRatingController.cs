6198698360096a27ba2698f1300e437022abc2ce
KasumiiUwU <p.khoa62@gmail.com>
2024-05-25 13:46:19 +0700

    commit

--- FuStudy_API/Controllers/Question/QuestionRatingController.cs
+++ FuStudy_API/Controllers/Question/QuestionRatingController.cs
@@ -19,30 +19,11 @@ public class QuestionRatingController : BaseController
         this._questionRatingService = questionRatingService;
     } 
     
-    [HttpGet("GetAllQuestions")]
-        public async Task<IActionResult> GetAllQuestionRatings()
-        {
-            var questionRatings = await _questionRatingService.GetAllQuestionsRatings();
-            return CustomResult("Data loaded!", questionRatings);
-        }
 
+    
 
-        [HttpGet("GetQuestionById/{id}")]
-        public async Task<IActionResult> GetQuestionById(long id)
-        {
-            var question = await _questionRatingService.GetQuestionCommentById(id);
-
-            if (question == null)
-            {
-                return CustomResult("Question not found", HttpStatusCode.NotFound);
-            }
-
-            return CustomResult("Data loaded!", question);
-
-        }
-
-    [HttpPost("CreateQuestion")]
-        public async Task<IActionResult> CreateQuestion([FromBody] QuestionRatingRequest questionRatingRequest)
+    [HttpPost("Like")]
+        public async Task<IActionResult> Like([FromBody] QuestionRatingRequest questionRatingRequest)
         {
             try
             {
@@ -71,34 +52,8 @@ public class QuestionRatingController : BaseController
             
         }
 
-        [HttpPost("UpdateQuestion/{questionId}")]
-        public async Task<IActionResult> UpdateQuestion(long questionId, [FromBody] QuestionRequest questionRequest)
-        {
-            if (!ModelState.IsValid)
-            {
-                return CustomResult(ModelState, HttpStatusCode.BadRequest);
-            }
-
-            try
-            {
-                var updateQuestion = await _questionRatingService.UpdateQuestionRating(questionRequest, questionId);
-                return CustomResult("Update successfully", updateQuestion);
-            }
-            catch (CustomException.DataNotFoundException e)
-            {
-                return CustomResult(e.Message, HttpStatusCode.NotFound);
-
-            }
-            catch (Exception exception)
-            {
-                return CustomResult(exception.Message, HttpStatusCode.InternalServerError);
-            }
-
-            
-        }
-
-        [HttpDelete("DeleteQuestion/{questionId}")]
-        public async Task<IActionResult> DeleteQuestion(long questionId)
+        [HttpDelete("Unlike/{questionId}")]
+        public async Task<IActionResult> Unlike(long questionId)
         {
             try
             {
