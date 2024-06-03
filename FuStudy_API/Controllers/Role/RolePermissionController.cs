8c7143e2d22e7850511e8bc0ef3bc274092355e8
Phạm Quốc Đạt <134580007+datpq-dev@users.noreply.github.com>
2024-06-03 22:31:16 +0700

    add CRUD Role

---
+++ FuStudy_API/Controllers/Role/RolePermissionController.cs
@@ -0,0 +1,12 @@
+﻿using CoreApiResponse;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FuStudy_API.Controllers.Role
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RolePermissionController : BaseController
+    {
+    }
+}
